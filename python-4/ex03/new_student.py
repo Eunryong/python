bd715fef5d67583875d6aba9aa5eb5fa6eb14166
Eunryong Lee <eunrlee@c2r8s3.42seoul.kr>
2024-05-10 13:10:21 +0900

    fix: python-4-ex03-post_init

--- python-4/ex03/new_student.py
+++ python-4/ex03/new_student.py
@@ -9,18 +9,18 @@ def generate_id() -> str:
 
 @dataclass
 class Student:
-    name: str
-    surname: str
+    name: string
+    surname: string
     active: bool = True
     login: str = field(init=False)
     id: str = field(init=False, default_factory=generate_id)
 
     def __post_init__(self):
-        self.login = self.name[0] + self.surname
+        self.login = self.name[0] + self.surname if len(self.name) > 0 else self.surname
 
 
 def main():
-    student = Student(name = "Edward", surname = "agle")
+    student = Student(name = "qwe", surname = "")
     print(student)
 
 if __name__ == "__main__":
